b5795ce87b368608c8df7f120872ac2fdce59bd7
LiuSuny <119320935+LiuSuny@users.noreply.github.com>
2023-12-19 01:26:15 +0300

    Event onclick and casting DONE.

--- Wpf_Basics/MainWindow.xaml.cs
+++ Wpf_Basics/MainWindow.xaml.cs
@@ -24,5 +24,43 @@ namespace Wpf_Basics
         {
             InitializeComponent();
         }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The description is - {this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WeldCheckBox.IsChecked = this.AssemblyCheckBox.IsChecked = this.PlasmaCheckBox.IsChecked 
+                = this.LaserCheckBox.IsChecked = this.PurchaseCheckBox.IsChecked = this.LatheCheckBox.IsChecked
+                = this.DrillCheckBox.IsChecked = this.FoldCheckBox.IsChecked = this.RollCheckBox.IsChecked
+                = this.SawCheckBox.IsChecked = false;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            //Once we tick our checkboxes the item appear in our length
+            this.LengthText.Text += ((CheckBox)sender).Content; //adding and casting(string)((CheckBox)sender) instead of be an object
+        }
+
+        private void FinishDropDown_SelectionChange(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NoteText == null) return;
+            var combo = (ComboBox)sender; //casting
+            var value = (ComboBoxItem)combo.SelectedValue; //casting 
+            this.NoteText.Text = (string)value.Content; //
+            //this.NoteText.Text = (string)((ComboBoxItem)((ComboBox)sender).SelectedValue).Content;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FinishDropDown_SelectionChange(this.FinishDropDown, null);
+        }
+
+        private void SuppplierNameText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.MassText.Text = this.SuppplierNameText.Text;
+        }
     }
 }
